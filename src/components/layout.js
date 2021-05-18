172697c10fa125f60845cd6019cf6b96a2b2abfd
Lior Beyderman <lior.bey95@gmail.com>
2021-05-18 10:20:03 -0400

    .

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,16 +2,18 @@ import React from 'react'
 import './base.css'
 import Container from './container'
 import Navigation from './navigation'
+import Footer from './footer'
 
 class Template extends React.Component {
   render() {
     const { children } = this.props
 
     return (
-      <Container>
-        <Navigation />
+      <body>
+      <Navigation />
         {children}
-      </Container>
+        <Footer/>
+      </body>
     )
   }
 }
